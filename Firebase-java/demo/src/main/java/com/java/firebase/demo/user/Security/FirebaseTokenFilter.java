854078383d084e10fccdfd4d1cbab457b2a2417c
garry014 <garychia14@gmail.com>
2024-09-23 22:26:03 +0800

    jwt authentication

---
+++ Firebase-java/demo/src/main/java/com/java/firebase/demo/user/Security/FirebaseTokenFilter.java
@@ -0,0 +1,57 @@
+package com.java.firebase.demo.user.Security;
+
+import java.io.IOException;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FirebaseTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = getTokenFromRequest(request);
+
+        if (token != null) {
+            try {
+                FirebaseToken firebaseToken = FirebaseAuth.getInstance().verifyIdToken(token);
+                PreAuthenticatedAuthenticationToken authentication = new PreAuthenticatedAuthenticationToken(
+                        firebaseToken, null, null);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (FirebaseAuthException e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    // @Override
+    // public void init(FilterConfig filterConfig) throws ServletException {
+    // }
+
+    // @Override
+    // public void destroy() {
+    // }
+}
