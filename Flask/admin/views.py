5b47ce098069fc60a84685316eafaafca30dd469
tzejunw <tzejun.wong.2023@scis.smu.edu.sg>
2024-10-05 22:00:56 +0800

    Changes to Admin View Tournaments Page

--- Flask/admin/views.py
+++ Flask/admin/views.py
@@ -9,12 +9,18 @@ from werkzeug.security import generate_password_hash
 import requests
 import jwt 
 
-@admin.route('/admin_view_tournaments')
-def admin_view_tournaments():
+@admin.route('/view_tournaments')
+def view_tournaments():
 
     api_url = 'http://localhost:8080/tournament/get/all'
     response = requests.get(api_url)  
     tournaments = response.json()  
 
     return render_template('admin/view_tournaments.html', tournaments=tournaments)
+
+@admin.route('/create_tournament')
+def create_tournament():
+
+ 
+    return render_template('admin/create_tournament.html')
 
