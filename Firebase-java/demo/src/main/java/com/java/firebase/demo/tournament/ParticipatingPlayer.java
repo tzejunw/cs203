dfeefbc5077fdcd67ba4b28a7b3b771240333760
petercegoh <petercegoh@gmail.com>
2024-10-16 13:54:11 +0800

    createPlayer, updatePlayerMatch, getPlayer
    FB route for participating player but not matches yet

---
+++ Firebase-java/demo/src/main/java/com/java/firebase/demo/tournament/ParticipatingPlayer.java
@@ -0,0 +1,12 @@
+package com.java.firebase.demo.tournament;
+import java.util.*;
+
+
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+public class ParticipatingPlayer {
+    public String userName;
+    public ArrayList<String> pastMatches;
+}
