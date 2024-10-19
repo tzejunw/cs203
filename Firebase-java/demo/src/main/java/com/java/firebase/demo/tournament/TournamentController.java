93a305b64f7265a9ffe81c89a2006b1c13005bd3
petercegoh <petercegoh@gmail.com>
2024-10-19 13:44:14 +0800

    /tournament/end

--- Firebase-java/demo/src/main/java/com/java/firebase/demo/tournament/TournamentController.java
+++ Firebase-java/demo/src/main/java/com/java/firebase/demo/tournament/TournamentController.java
@@ -34,6 +34,14 @@ public class TournamentController {
     // update inProgress field to false
     // this will only happen afeter stadnigns are updated. 
 
+<<<<<<< Updated upstream
+=======
+    @PostMapping("/tournament/end") // changes a field in the tournament FB documennt
+    public String endTournament(@RequestParam String tournamentName) throws InterruptedException, ExecutionException {
+        return tournamentService.endTournament(tournamentName);
+    }
+
+>>>>>>> Stashed changes
     @GetMapping("/tournament/get") // documentId is the user's tournamentName. The argument here determines what it expects as the key in Postman
     public Tournament getTournament(@RequestParam String tournamentName) throws InterruptedException, ExecutionException {
         return tournamentService.getTournament(tournamentName);
